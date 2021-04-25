4865641e1ec646ecbc83e4093f0a909c03588d50
Owais Idris <owaistnt@gmail.com>
2021-04-25 20:59:44 +0530

    Tests for View Model calculator

--- mwidgets/src/main/java/com/artsman/mwidgets/CalculatorWidget.kt
+++ mwidgets/src/main/java/com/artsman/mwidgets/CalculatorWidget.kt
@@ -13,6 +13,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.artsman.mwidgets.databinding.WidgetLayoutCalculatorBinding
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.math.BigDecimal
 
 /**
@@ -93,14 +95,31 @@ class CalculatorWidget : ConstraintLayout {
 }
 
 class CalculatorViewModel(){
-    private val mStates=MutableLiveData<States>()
-    fun subcribe(): LiveData<States> {
+    private val mStates= MutableStateFlow<States>(States.Display("0"))
+
+    private var _display=""
+    fun subscribe(): StateFlow<States> {
         return mStates
     }
 
     fun setAction(action: Actions) {
         when(action){
-            Actions.Start -> mStates.postValue(States.Display("0"))
+            Actions.Start -> mStates.value=(States.Display("0"))
+            is Actions.Input -> {
+                if (action.input.matches("[0-9]".toRegex())) {
+                    processNumberInput(action)
+                }
+            }
+        }
+    }
+
+    private fun processNumberInput(action: Actions.Input) {
+        if (_display.isEmpty()) {
+            mStates.value = States.Display("${action.input}")
+            _display += action.input
+        } else {
+            _display += action.input
+            mStates.value = States.Display("$_display")
         }
     }
 
@@ -109,6 +128,10 @@ class CalculatorViewModel(){
     }
 
     sealed class Actions{
+        data class Input(val input: String) : Actions() {
+
+        }
+
         object Start: Actions()
     }
 }
--- mwidgets/src/test/java/com/artsman/mwidgets/CalculatorViewModelTest.kt
+++ mwidgets/src/test/java/com/artsman/mwidgets/CalculatorViewModelTest.kt
@@ -2,30 +2,60 @@ package com.artsman.mwidgets
 
 import androidx.lifecycle.Observer
 import io.kotest.core.spec.style.StringSpec
-import io.mockk.clearMocks
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
 
 class CalculatorViewModelTest : StringSpec({
 
     lateinit var calculatorViewModel: CalculatorViewModel
 
-    val observer:Observer<CalculatorViewModel.States> = mockk<Observer<CalculatorViewModel.States>>()
+    val observer: Observer<CalculatorViewModel.States> = mockk<Observer<CalculatorViewModel.States>>()
+    val coroutineDispatcher = TestCoroutineDispatcher()
 
-    listener(InstantExecutorListener())
+    /*listener(InstantExecutorListener())*/
     beforeEach {
-        calculatorViewModel= CalculatorViewModel()
-        calculatorViewModel.subcribe().observeForever(observer)
+        Dispatchers.setMain(coroutineDispatcher)
+        calculatorViewModel = CalculatorViewModel()
     }
 
     afterEach {
-        calculatorViewModel.subcribe().removeObserver(observer)
         //clearMocks(observer)
     }
 
     "on start it must display 0"{
         calculatorViewModel.setAction(CalculatorViewModel.Actions.Start)
-        verify(atLeast = 1) { observer.onChanged(CalculatorViewModel.States.Display("0")) }
+        val subscription = calculatorViewModel.subscribe()
+        assert(subscription.value == CalculatorViewModel.States.Display("0"))
+    }
+
+    "on digit enter it must display number without zero prefix"{
+        coroutineDispatcher.runBlockingTest {
+            calculatorViewModel.setAction(CalculatorViewModel.Actions.Start)
+            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("0")
+            calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("1"))
+            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("1")
+            calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("2"))
+            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("12")
+
+        }
+    }
+
+    "on non digit it must not add to display"{
+        coroutineDispatcher.runBlockingTest {
+            calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("+"))
+            calculatorViewModel.subscribe().first() shouldNotBe  CalculatorViewModel.States.Display("+")
+        }
     }
 })
 
