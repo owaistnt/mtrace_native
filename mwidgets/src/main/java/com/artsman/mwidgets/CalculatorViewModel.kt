7dad669c751d8b738b19d1dd59da7c0a68d2ce57
Owais Idris <owaistnt@gmail.com>
2021-05-03 00:28:51 +0530

    Tests for View Model calculator

--- mwidgets/src/main/java/com/artsman/mwidgets/CalculatorViewModel.kt
+++ mwidgets/src/main/java/com/artsman/mwidgets/CalculatorViewModel.kt
@@ -1,25 +1,67 @@
 package com.artsman.mwidgets
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.util.*
 
-class CalculatorViewModel(){
+class CalculatorViewModel(private val calculator: Calculator): ViewModel(){
     private val mStates= MutableStateFlow<States>(States.Display("0"))
-
+    private val mInputStack= Stack<BigDecimal>()
+    private val mOperationStack= Stack<String>()
     private var _display=""
     fun subscribe(): StateFlow<States> {
         return mStates
     }
 
     fun setAction(action: Actions) {
-        when(action){
-            Actions.Start -> mStates.value=(States.Display("0"))
-            is Actions.Input -> {
-                if (action.input.matches("[0-9]".toRegex())) {
-                    processNumberInput(action)
+        viewModelScope.launch {
+            when(action){
+                Actions.Start -> mStates.value=(States.Display("0"))
+                is Actions.Input -> {
+                    if (action.input.matches("[0-9]".toRegex())) {
+                        processNumberInput(action)
+                    } else {
+                        if (mOperationStack.isEmpty()) {
+                            mOperationStack.push(action.input)
+                            if (mInputStack.isEmpty()) {
+                                mInputStack.push(_display.toBigDecimal())
+                                _display = ""
+                            }
+                            return@launch
+                        }
+                        var answer = mInputStack.peek();
+                        val operation = mOperationStack.pop()
+                        val input1 = mInputStack.pop()
+                        if (_display.isNotEmpty()) {
+                            val input2 = _display.toBigDecimal()
+                            answer = calculator.calculate(
+                                input1,
+                                input2,
+                                operation.getOperator()
+                            )
+                        }
+                        mInputStack.push(answer)
+                        if (action.input != "=") {
+                            mOperationStack.push(action.input)
+                        }
+                        _display = ""
+                        mStates.emit(States.Display(answer.toString()))
+                    }
                 }
             }
         }
+    }
+
+    private fun String.getOperator()=when(this){
+        "+"-> CalculatorWidget.OPERATOR.ADD
+        "-"-> CalculatorWidget.OPERATOR.SUB
+        "/"-> CalculatorWidget.OPERATOR.DIV
+        "*"-> CalculatorWidget.OPERATOR.PROD
+        else-> CalculatorWidget.OPERATOR.NONE
     }
 
     private fun processNumberInput(action: Actions.Input) {
--- mwidgets/src/test/java/com/artsman/mwidgets/CalculatorViewModelTest.kt
+++ mwidgets/src/test/java/com/artsman/mwidgets/CalculatorViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.artsman.mwidgets
 
 import androidx.lifecycle.Observer
+import app.cash.turbine.test
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
@@ -15,15 +17,18 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 class CalculatorViewModelTest : StringSpec({
 
     lateinit var calculatorViewModel: CalculatorViewModel
 
-
+    val testCoroutingDispatcher= TestCoroutineDispatcher()
     /*listener(InstantExecutorListener())*/
     beforeEach {
-        calculatorViewModel = CalculatorViewModel()
+        Dispatchers.setMain(testCoroutingDispatcher)
+        calculatorViewModel = CalculatorViewModel(Calculator())
     }
 
     afterEach {
@@ -37,13 +42,16 @@ class CalculatorViewModelTest : StringSpec({
     }
 
     "on digit enter it must display number without zero prefix"{
-        runBlocking {
-            calculatorViewModel.setAction(CalculatorViewModel.Actions.Start)
-            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("0")
-            calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("1"))
-            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("1")
-            calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("2"))
-            calculatorViewModel.subscribe().first() shouldBe CalculatorViewModel.States.Display("12")
+        testCoroutingDispatcher.runBlockingTest {
+            calculatorViewModel.subscribe().test {
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Start)
+                expectItem() shouldBe CalculatorViewModel.States.Display("0")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("1"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("1")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("2"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("12")
+
+            }
 
         }
     }
@@ -52,6 +60,33 @@ class CalculatorViewModelTest : StringSpec({
         runBlocking {
             calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("+"))
             calculatorViewModel.subscribe().first() shouldNotBe  CalculatorViewModel.States.Display("+")
+        }
+    }
+
+    "on simple calculation"{
+        testCoroutingDispatcher.runBlockingTest {
+            calculatorViewModel.subscribe().test {
+                expectItem() shouldBe CalculatorViewModel.States.Display("0")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("1"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("1")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("0"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("10")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("+"))
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("5"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("5")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("+"))
+                val expectedResult= expectItem()
+                expectedResult shouldBe CalculatorViewModel.States.Display("15")
+
+                //substracting 5 from it now
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("-"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("15")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("5"))
+                expectItem() shouldBe CalculatorViewModel.States.Display("5")
+                calculatorViewModel.setAction(CalculatorViewModel.Actions.Input("="))
+                expectItem() shouldBe CalculatorViewModel.States.Display("10")
+                cancel()
+            }
         }
     }
 })
